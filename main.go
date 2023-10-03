ac77eb26ff16cab7a7af3d257959f627427d035c
theo <lesgooo1993@proton.me>
2023-10-04 00:20:06 +0530

    param

--- main.go
+++ main.go
@@ -14,10 +14,19 @@ func hello(c *gin.Context) {
   c.String(http.StatusOK, "hello %s!\n", name)
 }
 
+func goodbye(c *gin.Context) {
+  name, err := c.Param("name")
+  if err != nil {
+    name = "friend"
+  }
+  c.String(http.StatusOK, "goodbye %s!\n", name)
+}
+
 func main() {
   router := gin.Default()
   router.GET("/", root)
   router.GET("/hello", hello)
+  router.GET("/goodbye/:name", goodbye)
   
   router.Run("localhost:3000")
 }
