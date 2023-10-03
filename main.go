bc5ceb2ba7ac81efdd79467ceda7a7897f992139
theo <lesgooo1993@proton.me>
2023-10-03 10:59:25 +0530

    added / route

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
   "github.com/gin-gonic/gin"
 )
 
+func root(c *gin.Context) {
+  c.String(http.StatusOK, "welcome!\n")
+}
+
 func hello(c *gin.Context) {
   c.String(http.StatusOK, "hello\n")
 }
 
 func main() {
   router := gin.Default()
+  router.GET("/", root)
   router.GET("/hello", hello)
   
   router.Run("localhost:3000")
